88c7c3c9fa61f045455507c5551a17d8a143137a
Shariful Islam Prince <54590868+001sharifulIslamnstu@users.noreply.github.com>
2020-03-18 06:18:58 -0700

    Add files via upload

---
+++ UVA problem solution UVa 494 - Kindergarten Counting Game solution.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    char a[10005];
+    int i,l,cnt;
+
+    while(gets(a))
+    {
+        l=strlen(a);
+        cnt=0;
+        for(i=0;i<l;i++)
+        {
+            if(isalpha(a[i]) && !isalpha(a[i+1]) cnt++;
+        }
+        printf("%d\n",cnt);
+    }
+    return 0;
+}
