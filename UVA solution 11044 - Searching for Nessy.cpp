28a4fa4e4c0ce4280ca0153ef40375d26fd379de
Shariful Islam Prince <54590868+001sharifulIslamnstu@users.noreply.github.com>
2020-03-18 06:18:03 -0700

    Add files via upload

---
+++ UVA solution 11044 - Searching for Nessy.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+int n,a,b,i;
+ scanf("%d",&n);
+while(n--)
+ {
+    scanf("%d %d",&a,&b);
+    printf("%d\n",(a/3)*(b/3));
+ }
+
+ return 0;
+}
