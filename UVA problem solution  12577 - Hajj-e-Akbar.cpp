6ed0e95b08b8e7ba4cfc6eacc901847df189ea28
Shariful Islam Prince <54590868+001sharifulIslamnstu@users.noreply.github.com>
2020-03-22 00:55:37 -0700

    Add files via upload

---
+++ UVA problem solution  12577 - Hajj-e-Akbar.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char input[20];
+
+    scanf("%s", input);
+
+    for (int t = 1; input[0] != '*'; ++t, scanf("%s", input))
+    {
+        if (input[0] == 'H')
+            printf("Case %d: Hajj-e-Akbar\n", t);
+        else
+            printf("Case %d: Hajj-e-Asghar\n", t);
+    }
+
+}
