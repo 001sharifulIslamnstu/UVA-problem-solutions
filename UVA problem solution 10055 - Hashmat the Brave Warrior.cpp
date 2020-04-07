21b25426b860a682e654f439d3ba16afa1388fd8
Shariful Islam Prince <54590868+001sharifulIslamnstu@users.noreply.github.com>
2020-04-07 08:28:36 -0700

    Add files via upload

---
+++ UVA problem solution 10055 - Hashmat the Brave Warrior.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long n,k;
+    while(scanf("%lld %lld",&n,&k)!= EOF)
+    {
+        printf("%lld\n",abs(n-k));
+    }
+
+}
