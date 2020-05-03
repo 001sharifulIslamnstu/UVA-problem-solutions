b321fa5d6b3c6387acc5b977c152a5e3a6996136
Shariful Islam Prince <54590868+001sharifulIslamnstu@users.noreply.github.com>
2020-05-03 02:45:07 -0700

    Add files via upload

---
+++ Uva problem solution 10014 - Simple calculations.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while(t--) {
+        int n, i;
+        double a, b, c;
+        scanf("%d %lf %lf", &n, &a, &b);
+        for(i = 0; i < n; i++) {
+            scanf("%lf", &c);
+            b -= 2*(n-i)*c;
+        }
+        b += n*a;
+        printf("%.2lf\n", b/(n+1));
+        if(t)
+            puts("");
+    }
+    return 0;
+}
